419a60e3d3f868e26f814bae9dc5603552534961
Yoshiki Shibukawa <yoshiki@shibu.jp>
2022-11-22 22:21:36 +0900

    build webui

---
+++ docs/_next/static/jJtcEcGK87A19FcsdoOnp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/eb1842f2-10dbaeccb280c8f0.js","static/chunks/914-d00431f8ce4eb996.js","static/chunks/pages/index-ad07b583e6383499.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
